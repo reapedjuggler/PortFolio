57cbaa7f6e045c0bb90de2fc677904a0eee14f62
reapedjuggler <tomarvibhav55@gmail.com>
2021-07-17 18:13:26 +0530

    upd skills
    Signed-off-by: reapedjuggler <tomarvibhav55@gmail.com>

--- src/components/Nav-items/About/About.js
+++ src/components/Nav-items/About/About.js
@@ -1,90 +1,162 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import Image from 'react-bootstrap/Image'
-import Profile from '../../../assets/img/profile/profile.jpg'
-import "./about.css"
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Profile from "../../../assets/img/profile/profile.jpg";
+import "./about.css";
 
 function About() {
-    return ( 
-        <div id = "about">
-            <div className = "about">
-            <h1 className="pt-3 text-center font-details pb-3"> ABOUT ME! </h1>
-           
-            <Container>
+	return (
+		<div id="about">
+			<div className="about">
+				<h1 className="pt-3 text-center font-details pb-3"> ABOUT ME! </h1>
 
-                <Row className="pt-3 pb-5 align-items-center"> 
-                    {/* Profile Pic */}
-                    <Col xs={12} md = {6}>
-                        <Row className = "justify-content-center mb-2 mr-2 ">
-                             <Image className="profile justify-content-end" src={Profile} thumbnail />
-                        </Row>
-                    </Col>
-  
-                    {/* -- Buttons -- */}
-                    <Col>
-                        <Row className=" align-items-start p-2 my-details rounded"> 
-                          <div className = "desc">
-                                <h3><strong>Hello folk! I'm Vibhav</strong></h3>
-                    
-                                <br /> A curious programmer with an endless appetite for learning, I'm a Sport Programmer and a Web Developer from New Delhi, India.
-                                Currently I am exploring stuff as an undergraduate in Computer Science Engineering at Jaypee Institute of Information Technology Noida.
-                                <br /> <br />
-                                I am always passionate about helping people through my problem solving skills and thus, I believe in growing as a team.
-                                <br /> <br />
-                                Along with Sport Programming, Reading Books, Watching Anime, Chess, Speed-Cubing, Table-Tennis and Music are my other hobbies.
-                                <br /> <br /> Being always involved in something, I am always curious to learn about new technology and leverage it to solve real world problems.
-                                <br />Feel free to contact me through twitter, email, linkedIn. I would be glad if I can help you in any possible way.
-                                <br />
-                
-                                <Col className="d-flex justify-content-center flex-wrap">
-                                
-                                    <div>
-                                        <a href="https://drive.google.com/file/d/158wzFCSFyz5H6-N1CX6yshGO8FLbQeu3/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                                        <Button className="m-2" variant="outline-primary"  size = "lg">
-                                            My Resume
-                                        </Button>
-                                        </a>
-                                    </div>
-                                    
-                                    <div>
-                                        <a href="https://github.com/reapedjuggler" target="_blank" rel="noopener noreferrer">
-                                        <Button className="m-2 fab fa-github-square" variant="outline-dark" size = "lg">
-                                            &nbsp; GitHub  
-                                        </Button>
-                                        </a>
-                                    </div>
-                                    
-                                    <div>
-                                        <a href="https://www.linkedin.com/in/vibhav-tomar-883282178/" target="_blank" rel="noopener noreferrer">
-                                        <Button className="m-2" variant="outline-info" size = "lg">
-                                            LinkedIn
-                                        </Button>
-                                        </a>
-                                    </div>
-        
-                                    <div>
-                                        <a href = "https://twitter.com/reapedjuggler" target="_blank" rel="noopener noreferrer">
-                                        <Button className="m-2 fab fa-twitter" variant = "outline-primary" size = "lg">
-                                            &nbsp; Twitter
-                                        </Button>
-                                        </a>
-                                    </div>
+				<Container>
+					<Row className="pt-3 pb-5 align-items-center">
+						{/* Profile Pic */}
+						<Col xs={12} md={6}>
+							<Row className="justify-content-center mb-2 mr-2 ">
+								<Image
+									className="profile justify-content-end"
+									src={Profile}
+									thumbnail
+								/>
+							</Row>
+						</Col>
 
-                                </Col>
+						{/* -- Buttons -- */}
+						<Col>
+							<Row className=" align-items-start p-2 my-details rounded">
+								<div className="desc">
+									<h3>
+										<strong>Hello folk! I'm Vibhav</strong>
+									</h3>
+									<br /> A curious programmer with an endless appetite for
+									learning, I'm a Sport Programmer and a Web Developer from New
+									Delhi, India. Currently I am exploring stuff as an
+									undergraduate in Computer Science Engineering at Jaypee
+									Institute of Information Technology Noida.
+									<br /> <br />
+									I am always passionate about helping people through my problem
+									solving skills and thus, I believe in growing as a team.
+									<br /> <br />
+									Along with Sport Programming, Reading Books, Watching Anime,
+									Chess, Speed-Cubing, Table-Tennis and Music are my other
+									hobbies.
+									<br /> <br /> Being always involved in something, I am always
+									curious to learn about new technology and leverage it to solve
+									real world problems.
+									<br />
+									Feel free to contact me through twitter, email, linkedIn. I
+									would be glad if I can help you in any possible way.
+									<br />
+									<Col className="d-flex justify-content-center flex-wrap">
+										<div>
+											<a
+												href="https://drive.google.com/file/d/158wzFCSFyz5H6-N1CX6yshGO8FLbQeu3/view?usp=sharing"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2"
+													variant="outline-primary"
+													size="lg"
+												>
+													My Resume
+												</Button>
+											</a>
+										</div>
 
-                            </div>
-                        </Row>
+										<div>
+											<a
+												href="https://github.com/reapedjuggler"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2 fab fa-github-square"
+													variant="outline-dark"
+													size="lg"
+												>
+													&nbsp; GitHub
+												</Button>
+											</a>
+										</div>
 
-                    </Col>
-                </Row>
-            </Container>
-            
-            </div>
-        </div>
-    )
+										<div>
+											<a
+												href="https://www.linkedin.com/in/vibhav-tomar-883282178/"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2"
+													variant="outline-info"
+													size="lg"
+												>
+													LinkedIn
+												</Button>
+											</a>
+										</div>
+
+										<div>
+											<a
+												href="https://twitter.com/reapedjuggler"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2 fab fa-twitter"
+													variant="outline-primary"
+													size="lg"
+												>
+													&nbsp; Twitter
+												</Button>
+											</a>
+										</div>
+
+										<div>
+											<a
+												href="https://codeforces.com/profile/s0ber_"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2"
+													variant="outline-info"
+													size="lg"
+												>
+													&nbsp; CodeForces
+												</Button>
+											</a>
+										</div>
+
+										<div>
+											<a
+												href="https://www.codechef.com/users/reaped_juggler"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<Button
+													className="m-2 fab"
+													variant="outline-primary"
+													size="lg"
+												>
+													&nbsp; Code Chef
+												</Button>
+											</a>
+										</div>
+									</Col>
+								</div>
+							</Row>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- src/components/Nav-items/tech/data.js
+++ src/components/Nav-items/tech/data.js
@@ -11,136 +11,151 @@ import L_MONGODB from "../../../assets/img/skills/mongodb.svg";
 import L_POSTGRESQL from "../../../assets/img/skills/postgresql.svg";
 import L_GIT from "../../../assets/img/skills/git-icon.svg";
 import L_HEROKU from "../../../assets/img/skills/heroku.svg";
+import L_DOCKER from "../../../assets/img/lfVWBmiW_400x400.png";
+import L_DigitalOcean from "../../../assets/img/index.jpeg";
 import L_JAVASCRIPT from "../../../assets/img/skills/javascript.svg";
-import L_JQUERY from '../../../assets/img/skills/jqeury.png'
-import L_MYSQL from '../../../assets/img/skills/mysql.png'
-import CPP from '../../../assets/img/skills/cpp.jpg';
-import reactRedux from '../../../assets/img/skills/react-redux.png';
+import L_JQUERY from "../../../assets/img/skills/jqeury.png";
+import L_MYSQL from "../../../assets/img/skills/mysql.png";
+import CPP from "../../../assets/img/skills/cpp.jpg";
+import reactRedux from "../../../assets/img/skills/react-redux.png";
 
 export const tech = {
+	frontend: [
+		{
+			link: "https://en.wikipedia.org/wiki/HTML5",
+			alt: "HTML 5",
+			imgSrc: L_HTML5,
+			name: "HTML5",
+		},
 
-    frontend: [{
-            link: "https://en.wikipedia.org/wiki/HTML5",
-            alt: "HTML 5",
-            imgSrc: L_HTML5,
-            name: "HTML5",
-        },
+		{
+			link: "https://en.wikipedia.org/wiki/CSS",
+			alt: "CSS 3",
+			imgSrc: L_CSS3,
+			name: "CSS3",
+		},
 
-        {
-            link: "https://en.wikipedia.org/wiki/CSS",
-            alt: "CSS 3",
-            imgSrc: L_CSS3,
-            name: "CSS3",
-        },
+		{
+			link: "https://jQuery.com",
+			alt: "jQuery",
+			imgSrc: L_JQUERY,
+			name: "jQuery",
+		},
 
-        {
-            link: "https://jQuery.com",
-            alt: "jQuery",
-            imgSrc: L_JQUERY,
-            name: "jQuery",
-        },
+		{
+			link: "https://getbootstrap.com/",
+			alt: "Bootstrap",
+			imgSrc: L_BOOTSTRAP,
+			name: "Bootstrap",
+		},
 
+		{
+			link: "https://reactjs.org/",
+			alt: "React JS",
+			imgSrc: L_REACT,
+			name: "React JS",
+		},
 
-        {
-            link: "https://getbootstrap.com/",
-            alt: "Bootstrap",
-            imgSrc: L_BOOTSTRAP,
-            name: "Bootstrap",
-        },
+		{
+			link: "https://react-bootstrap.github.io/",
+			alt: "React Bootstrap",
+			imgSrc: L_REACT_BOOTSTRAP,
+			name: "React Bootstrap",
+		},
 
-        {
-            link: "https://reactjs.org/",
-            alt: "React JS",
-            imgSrc: L_REACT,
-            name: "React JS",
-        },
+		{
+			link: "https://styled-components.com/",
+			alt: "styled-components",
+			imgSrc: L_STYLED_COMPONENTS,
+			name: "styled-components",
+		},
 
-        {
-            link: "https://react-bootstrap.github.io/",
-            alt: "React Bootstrap",
-            imgSrc: L_REACT_BOOTSTRAP,
-            name: "React Bootstrap",
-        },
+		{
+			link: "https://redux.js.org/",
+			alt: "React-Redux",
+			imgSrc: reactRedux,
+			name: "React-Redux",
+		},
+	],
 
-        {
-            link: "https://styled-components.com/",
-            alt: "styled-components",
-            imgSrc: L_STYLED_COMPONENTS,
-            name: "styled-components",
-        },
+	backend: [
+		{
+			link: "https://nodejs.org/en/",
+			alt: "Node.js",
+			imgSrc: L_NODE_JS,
+			name: "Node.js",
+		},
+		{
+			link: "https://expressjs.com/",
+			alt: "Express",
+			imgSrc: L_EXPRESS,
+			name: "Express",
+		},
 
-        {
-            link:"https://redux.js.org/",
-            alt: "React-Redux",
-            imgSrc: reactRedux,
-            name: "React-Redux"
-        }
+		{
+			link: "https://www.postgresql.org/",
+			alt: "PostgreSQL",
+			imgSrc: L_POSTGRESQL,
+			name: "PostgreSQL",
+		},
 
+		{
+			link: "https://www.mysql.com/",
+			alt: "MS-SQL",
+			imgSrc: L_MYSQL,
+			name: "MY-SQL",
+		},
+		{
+			link: "https://www.mongodb.com/",
+			alt: "MongoDB",
+			imgSrc: L_MONGODB,
+			name: "MongoDB",
+		},
+	],
 
-    ],
+	programmingLanguages: [
+		{
+			link: "https://www.javascript.com/",
+			alt: "JavaScript",
+			imgSrc: L_JAVASCRIPT,
+			name: "JavaScript",
+		},
 
-    backend: [{
-            link: "https://nodejs.org/en/",
-            alt: "Node.js",
-            imgSrc: L_NODE_JS,
-            name: "Node.js",
-        },
-        {
-            link: "https://expressjs.com/",
-            alt: "Express",
-            imgSrc: L_EXPRESS,
-            name: "Express",
-        },
-        
-        {
-            link: "https://www.postgresql.org/",
-            alt: "PostgreSQL",
-            imgSrc: L_POSTGRESQL,
-            name: "PostgreSQL",
-        },
+		{
+			link: "https://www.cplusplus.com/reference/",
+			alt: "C++",
+			imgSrc: CPP,
+			name: "C++",
+		},
+	],
 
-        {
-            link: "https://www.mysql.com/",
-            alt: "MS-SQL",
-            imgSrc: L_MYSQL,
-            name: "MY-SQL",
-        },
-        {
-            link: "https://www.mongodb.com/",
-            alt: "MongoDB",
-            imgSrc: L_MONGODB,
-            name: "MongoDB",
-        },
-    ],
+	other: [
+		{
+			link: "https://git-scm.com/",
+			alt: "GIT",
+			imgSrc: L_GIT,
+			name: "GIT",
+		},
 
-    programmingLanguages: [{
-            link: "https://www.javascript.com/",
-            alt: "JavaScript",
-            imgSrc: L_JAVASCRIPT,
-            name: "JavaScript",
-        },
+		{
+			link: "https://www.heroku.com/",
+			alt: "Heroku",
+			imgSrc: L_HEROKU,
+			name: "Heroku",
+		},
 
-        {
-            link: "https://www.cplusplus.com/reference/",
-            alt: "C++",
-            imgSrc: CPP,
-            name: "C++",
-        },
+		{
+			link: "https://docs.docker.com/",
+			alt: "Docker",
+			imgSrc: L_DOCKER,
+			name: "Docker",
+		},
 
-    ],
-
-    other: [{
-            link: "https://git-scm.com/",
-            alt: "GIT",
-            imgSrc: L_GIT,
-            name: "GIT",
-        },
-
-        {
-            link: "https://www.heroku.com/",
-            alt: "Heroku",
-            imgSrc: L_HEROKU,
-            name: "Heroku",
-        },
-    ],
+		{
+			link: "https://www.digitalocean.com/",
+			alt: "DigitalOcean",
+			imgSrc: L_DigitalOcean,
+			name: "DigitalOcean",
+		},
+	],
 };
